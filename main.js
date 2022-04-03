8791135882f4ce8908ac4f9a23640cb95c6e71b4
Sohan-Sunkari <88518634+Sohan-Sunkari@users.noreply.github.com>
2022-04-03 16:06:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+var title = "My first console web project by ";
+console.log(title);
+
+var firstName = "Sohan";
+var lastName = " Sunkari";
+var bothNames = firstName.concat(lastName);
+console.log(bothNames);
+console.log(firstName);
+console.log(lastName);
+
+var space = " "
+console.log(space)
+//answer 1
+var a = 5;
+var b = 10;
+var answerOne = a+b;
+//answer 1
+
+//console the additions for number 1
+var plus = "+";
+var aa = "5";
+var bb = "10";
+var aaplusbb = aa.concat(plus.concat(bb));
+console.log(answerOne)
+//console the additions for number 1
+var space = " "
+console.log(space)
+
+
+//answer 2
+var c = 5;
+var d = 10;
+var answerTwo = d/2;
+//answer 2
+
+//consoles for number 2
+var div = "÷"
+var cc = "5"
+var dd = "10"
+var ccc = "2"
+var dddivccc = dd.concat(div.concat(ccc));
+console.log(answerTwo)
+//consoles for number 2
+
+var space = " "
+console.log(space)
+
+var three = 5*10;
+console.log(three)
+
+var space = " "
+console.log(space)
+
+var four = 10/5;
+console.log(four);
+
+
+var space = " "
+console.log(space)
+
+
+console.log("The Info")
+
+
+
+var isss = "What is Visual Studio Code or Vss?Visual Studio Code, also commonly referred to as VS Code, is a source-code editor made by Microsoft for Windows, Linux and macOS. Features include support for debugging, syntax highlighting, intelligent code completion, snippets, code refactoring, and embedded Git.Visual Studio Code was first announced on April 29, 2015, by Microsoft at the 2015 Build conference. A preview build was released shortly thereafter. On November 18, 2015, the source of Visual Studio Code was released under the MIT License, and made available on GitHub. Extension support was also announced. On April 14, 2016, Visual Studio Code graduated from the public preview stage and was released to the Web. Microsoft has released most of Visual Studio Code's source code on GitHub under the permissive MIT License, while the releases by Microsoft are proprietary freeware.Visual Studio Code is a source-code editor that can be used with a variety of programming languages, including Java, JavaScript, Go, Node.js, Python and C++. It is based on the Electron framework, which is used to develop Node.js Web applications that run on the Blink layout engine. Visual Studio Code employs the same editor component (codenamed 'Monaco') used in Azure DevOps (formerly called Visual Studio Online and Visual Studio Team Services). Out of the box, Visual Studio Code includes basic support for most common programming languages. This basic support includes syntax highlighting, bracket matching, code folding, and configurable snippets. Visual Studio Code also ships with IntelliSense for JavaScript, TypeScript, JSON, CSS, and HTML, as well as debugging support for Node.js. Support for additional languages can be provided by freely available extensions on the VS Code Marketplace. An orange version of the Visual Studio Code logo for the insiders version of Visual Studio Code Visual Studio Code Insiders logo Instead of a project system, it allows users to open one or more directories, which can then be saved in workspaces for future reuse. This allows it to operate as a language-agnostic code editor for any language. It supports many programming languages and a set of features that differs per language. Unwanted files and folders can be excluded from the project tree via the settings. Many Visual Studio Code features are not exposed through menus or the user interface but can be accessed via the command palette. Visual Studio Code can be extended via extensions, available through a central repository. This includes additions to the editor and language support. A notable feature is the ability to create extensions that add support for new languages, themes, and debuggers, perform static code analysis, and add code linters using the Language Server Protocol. Source control is a built-in feature of Visual Studio Code. It has a dedicated tab inside of the menu bar where you can access version control settings and view changes made to the current project. To use the feature you must link Visual Studio Code to any supported version control system (Git, Apache Subversion, Perforce, etc.). This allows you to create repositories as well as to make push and pull requests directly from the Visual Studio Code program. Visual Studio Code includes multiple extensions for FTP, allowing the software to be used as a free alternative for web development. Code can be synced between the editor and the server, without downloading any extra software. Visual Studio Code allows users to set the code page in which the active document is saved, the newline character, and the programming language of the active document. This allows it to be used on any platform, in any locale, and for any given programming language. Visual Studio Code collects usage data and sends it to Microsoft, although this can be disabled. Due to the open-source nature of the application, the telemetry code is accessible to the public, who can see exactly what is collected.In the 2016 Developers Survey of Stack Overflow, Visual Studio Code ranked #13 among the top popular development tools, with only 7% of the 47,000 respondents using it. Two years later, however, Visual Studio Code achieved the #1 spot, with 35% of the 75,000 respondents using it. In the 2019 Developers Survey, Visual Studio Code was also ranked #1, with 50% of the 87,000 respondents using it. he 2021 Developers Survey, Visual Studio Code continues to be ranked #1, with 70% of the 82,000 respondents using it.";
+console.log(isss);
+
+var sssi = "The developer console is a tool which logs the information associated with a web page, such as JavaScript, network requests, and security errors.The information displayed in the developer console can be extremely helpful for the Airtable support team when we're trying to figure out how to solve an issue. In the course of troubleshooting your issue, the Airtable support team may ask you to take a screenshot of the developer console.";
+console.log(sssi)
+
+
